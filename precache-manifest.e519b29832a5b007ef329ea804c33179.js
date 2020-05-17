8ce1653ce85997248ccdda394985c3aed8426f4e
nektuty <nektuty74@gmail.com>
2020-05-17 17:20:03 +0500

    first version deployed

---
+++ precache-manifest.e519b29832a5b007ef329ea804c33179.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3821fc92f03523a91a7cf1b62512b1",
+    "url": "/gentree-frontend/index.html"
+  },
+  {
+    "revision": "29acc2d9f5b4786f314b",
+    "url": "/gentree-frontend/static/css/3.c6db1617.chunk.css"
+  },
+  {
+    "revision": "8316575a615ede28a33e",
+    "url": "/gentree-frontend/static/css/4.088c84dd.chunk.css"
+  },
+  {
+    "revision": "8c94300636b9a6804584",
+    "url": "/gentree-frontend/static/css/5.a5cdaaa9.chunk.css"
+  },
+  {
+    "revision": "be138dec61b36272c74d",
+    "url": "/gentree-frontend/static/css/main.5dab3bc1.chunk.css"
+  },
+  {
+    "revision": "69758031230fa6022a56",
+    "url": "/gentree-frontend/static/js/2.02351f7d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gentree-frontend/static/js/2.02351f7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29acc2d9f5b4786f314b",
+    "url": "/gentree-frontend/static/js/3.3bf8e03f.chunk.js"
+  },
+  {
+    "revision": "8316575a615ede28a33e",
+    "url": "/gentree-frontend/static/js/4.1da97a33.chunk.js"
+  },
+  {
+    "revision": "8c94300636b9a6804584",
+    "url": "/gentree-frontend/static/js/5.58e4e773.chunk.js"
+  },
+  {
+    "revision": "be138dec61b36272c74d",
+    "url": "/gentree-frontend/static/js/main.1621d805.chunk.js"
+  },
+  {
+    "revision": "7ae3c81bac4a53d17713",
+    "url": "/gentree-frontend/static/js/runtime-main.f7ee1798.js"
+  },
+  {
+    "revision": "112978840e32567db7f35080d8f931e8",
+    "url": "/gentree-frontend/static/media/spinner.11297884.svg"
+  }
+]);
